54ff196cceb1be56d95b71e9b968037065bbe6c0
Abhishek Charpe <I759407@cxloyaltycorp.com>
2025-08-03 19:11:09 +0530

    added changes

--- Log.Analyzer.Host/Program.cs
+++ Log.Analyzer.Host/Program.cs
@@ -15,6 +15,7 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
+        //abc
         var environment = args.FirstOrDefault() ?? "qa";
         Console.WriteLine($"Running environment: {environment}");
 
